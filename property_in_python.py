72aa5bc0513711c80d6d1bdf59a98463bbb00db8
Harsh Gupta <harsh@Harshs-MacBook-Pro.local>
2019-05-25 23:43:12 +0530

    property example added.

--- property_in_python.py
+++ property_in_python.py
@@ -1,34 +1,33 @@
 # Property function returns an object that proxies the requests
 # to set or access the attribute value through the methods specified.
 
+# class Silly:
+#     def __init__(self):
+#         """
+#         Initialize silly with a default value.
+#         """
+#         self._silly = "silly"
 
-class Silly:
-    def __init__(self):
-        """
-        Initialize silly with a default value.
-        """
-        self._silly = "silly"
+#     def _get_silly(self):
+#         """
+#         Getting the value of proprty silly.
+#         """
+#         print("You are getting silly")
+#         return self._silly
 
-    def _get_silly(self):
-        """
-        Getting the value of proprty silly.
-        """
-        print("You are getting silly")
-        return self._silly
+#     def _set_silly(self, value):
+#         """
+#         Setting the value of proprty silly.
+#         """
+#         print("You are making silly {}".format(value))
+#         self._silly = value
 
-    def _set_silly(self, value):
-        """
-        Setting the value of proprty silly.
-        """
-        print("You are making silly {}".format(value))
-        self._silly = value
+#     def _del_silly(self):
+#         print("Whoah, you killed silly!")
+#         del self._silly
 
-    def _del_silly(self):
-        print("Whoah, you killed silly!")
-        del self._silly
-
-    silly = property(_get_silly, _set_silly, _del_silly,
-                     "This is a silly property")
+#     silly = property(_get_silly, _set_silly, _del_silly,
+#                      "This is a silly property")
 
 
 class SillyUsingDecorator:
@@ -36,6 +35,9 @@ class SillyUsingDecorator:
     This class does the same thing as above but it is 
     implemented using decorators.
     """
+
+    def __init__(self):
+        self.silly = "silly"
 
     @property
     def silly(self):
